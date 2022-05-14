3682aed2fdd61a02b27281abe8e871528bfee408
맹주영 <mjy9088@naver.com>
2022-05-15 01:31:01 +0900

    add basic test

---
+++ test/case0/test.mk
@@ -0,0 +1,12 @@
+include ../common/prelude.mk
+
+SRCS := $(shell cd src && find . -maxdepth 1 -type f -name "*.c")
+
+CC ?= clang
+CFLAGS ?= -Wall -Wextra -Werror -std=c99 -pedantic
+
+test: $(addprefix obj/, $(SRCS:.c=.o))
+obj/%.o: src/%.c | obj
+	$Q$(CC) $(CFLAGS) -c -o $@ $<
+obj:
+	$Qmkdir obj
