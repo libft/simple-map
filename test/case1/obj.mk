fe982b2b17528a7132f34e9c62101456c9ff7630
맹주영 <mjy9088@naver.com>
2022-05-17 00:20:33 +0900

    fix test bug

--- test/case1/obj.mk
+++ test/case1/obj.mk
@@ -2,7 +2,7 @@ include ../../common/prelude.mk
 
 CC := clang
 CFLAGS := -Wall -Wextra -Werror -std=c99 -pedantic
-CPPFLAGS := -I../../target/include
+CPPFLAGS := -I../../target
 ARFLAGS := cr$(if $(filter 1,$(V) $(VERBOSE)),v,s)
 
 SRCS := $(shell find ../src -maxdepth 1 -type f -name "*.c")
@@ -23,3 +23,10 @@ undefined.a: $(patsubst ../src/%.c,%.undefined.o,$(SRCS))
 	$Q$(CC) $(CFLAGS) $(CPPFLAGS) -fsanitize=memory -c -o $@ $<
 %.undefined.o: ../src/%.c
 	$Q$(CC) $(CFLAGS) $(CPPFLAGS) -fsanitize=undefined -c -o $@ $<
+
+lib.none.a: $(patsubst ../src/%.c,%.none.o,$(SRCS))
+lib.address.a: $(patsubst ../src/%.c,%.address.o,$(SRCS))
+lib.memory.a: $(patsubst ../src/%.c,%.memory.o,$(SRCS))
+lib.undefined.a: $(patsubst ../src/%.c,%.undefined.o,$(SRCS))
+%.a:
+	$Q$(AR) $(ARFLAGS) $@ $^
